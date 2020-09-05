d2ac9bd4aa11da904679d722b38e914a0c5d67a0
helaliferid <helaliferid@gmail.com>
2020-09-05 10:12:39 +0200

    Scaffold and Navigation

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'CodeForTeen Pets' });
 });
 
+router.get('/dogs', function(req, res, next) {
+    res.render('dogs', { title: 'CodeForTeen Pets' });
+});
+
+router.get('/cats', function(req, res, next) {
+    res.render('cats', { title: 'CodeForTeen Pets' });
+});
+
+router.get('/hamsters', function(req, res, next) {
+    res.render('hamsters', { title: 'CodeForTeen Pets' });
+});
+
+router.get('/about', function(req, res, next) {
+    res.render('about', { title: 'CodeForTeen Pets' });
+});
+
+router.get('/contact', function(req, res, next) {
+    res.render('contact', { title: 'CodeForTeen Pets' });
+});
+
 module.exports = router;
